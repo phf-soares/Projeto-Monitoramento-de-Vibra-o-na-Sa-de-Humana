5a41a5bc946d82cf849d99f5f51a836faa2dfbe8
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-11-10 23:28:54 -0300

    Initialize Const

---
+++ README.md
@@ -0,0 +1,62 @@
+# Arduino IDE sensor_bno055 e ESP32
+Protocolo I2C entre:
+
+1) Módulo sensor BNO055
+
+![image](https://github.com/user-attachments/assets/376cb56f-e07f-475c-9e07-f9a22ab78216)
+
+2) ESP32-WROOM-32D OU DEVKIT-V1
+
+
+![image](https://github.com/user-attachments/assets/45c2b9af-bc6b-4dd6-a4b6-040efd92e512)
+
+Montagem:
+![image](https://github.com/user-attachments/assets/1353824b-a9ea-47d3-8f77-ff2a4f163949)
+
+Protocolo SPI entre ESP32 e:
+
+3) Módulo SD:
+
+
+![image](https://github.com/user-attachments/assets/3b610a80-6c44-49eb-a397-15c0167fdf9d)
+
+Montagem:
+![image](https://github.com/user-attachments/assets/ac5387a5-40da-4f57-bd0b-2beca50ef739)
+Feito por mim: https://wokwi.com/projects/434601176080578561
+
+Alimentado pelo USB.
+
+Protocolo UART entre ESP32 e:
+
+4) Módulo GSM:
+
+![image](https://github.com/user-attachments/assets/0804a8ed-badc-4e53-b7c5-abcb6c645db7)
+
+![image](https://github.com/user-attachments/assets/b6575555-592b-4a55-b821-86ea62310f29)
+
+![image](https://github.com/user-attachments/assets/ad93fddd-e24f-4fb0-9c66-78965fc42f0e)
+
+https://www.youtube.com/watch?v=THCJWWsyh10
+
+
+ALIMENTAÇÃO:
+
+- Alimentação portátil por bateria e carregador da mesma:
+
+![image](https://github.com/user-attachments/assets/2655918b-aa83-43d6-be84-c99d01dfc830)
+
+https://www.usinainfo.com.br/carregador-de-bateria/shield-v3-carregador-de-bateria-18650-com-usb-saida-5v-3v-e-protecao-de-sobrecarga-ou-descarga-excessiva-5319.html
+
+![image](https://github.com/user-attachments/assets/20aa0b39-3b69-42b3-8f20-2ac998f9312b)
+
+https://youtu.be/J86yQlJO4w0?si=M3n3m-WgvZsvyjYb
+
+- Bateria recarregável:
+
+Bateria 18650 Lithium (Li-ion) 3.7V / 2600 mAh - FLEXGOLD FX-L2600 Flat-top
+
+![image](https://github.com/user-attachments/assets/967cf584-5a9a-4877-998e-19b579f174dc)
+
+
+
+
---
+++ bno_i2c/acelerometer_sensor/acelerometer_sensor.ino
@@ -0,0 +1,123 @@
+#include <Wire.h>
+
+const uint8_t POWER_MODE_ADDR = 0x3E;
+const uint8_t POWER_NORMAL_MODE = 0x00;
+const uint8_t POWER_LOW_MODE = 0X01;
+const uint8_t POWER_SUSPEND_MODE = 0X02;
+const uint8_t OPERATION_MODE_ADDR = 0x3D;
+const uint8_t OPERATION_NDOF = 0x0C;
+const uint8_t Operation_ACCONLY = 0X01;
+const uint8_t ACCEL_DATA_X_LSB_ADDR = 0x08;
+const uint8_t LINEAR_ACCEL_DATA_X_LSB_ADDR = 0x28;
+const uint8_t MAG_DATA_X_LSB_ADDR = 0X0E;
+const uint8_t SELFTEST_RESULT_ADDR = 0x36;
+
+float Yaw, Roll, Pitch, magx, magy, magz, accx, accy, accz, gyrox, gyroy, gyroz, q0, q1, q2, q3, Roll2, Pitch2, Yaw2, LIAx, LIAy, LIAz, GRVx, GRVy, GRVz;
+
+const uint8_t I2C_ADDR = 0x29;  //0x29 endereco i2c
+const unsigned long BAUD_RATE = 115200; //taxa de trasmissao em bits por segundo (bps)
+const uint32_t SETTING_TIME_MS = 500;
+
+void POST_test() {  
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(SELFTEST_RESULT_ADDR);
+  Wire.endTransmission(false);
+  Wire.requestFrom(I2C_ADDR, 1, true);
+  uint8_t POST_result = Wire.read();
+  bool bit0_ACC_result = ((POST_result>>0)&1);
+  if (bit0_ACC_result == 0) {
+    Serial.print("ACELEROMETRO FALHOU");
+    // mandar alerta SMS;
+    delay(SETTING_TIME_MS);
+    exit(0);
+  } else
+  delay(SETTING_TIME_MS);
+}
+
+void Sensor_config() {
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(POWER_MODE_ADDR);
+  Wire.write(POWER_NORMAL_MODE);
+  Wire.endTransmission();
+  delay(SETTING_TIME_MS);
+
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(OPERATION_MODE_ADDR);
+  Wire.write(OPERATION_NDOF);
+  Wire.endTransmission();
+  delay(SETTING_TIME_MS);
+}
+
+void setup() {
+  // Transmissao
+  Serial.begin(BAUD_RATE);
+  delay(SETTING_TIME_MS);
+
+  // Inicio protocolo I2C
+  Wire.begin();  
+  delay(SETTING_TIME_MS);
+
+  // Power On Self Test (POST)
+  POST_test();
+
+  // Configuracao sensor
+  Sensor_config();
+}
+
+void loop() {  
+  // Leitura sequencial dos sensores (registradores)
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(ACCEL_DATA_X_LSB_ADDR);
+  Wire.endTransmission(false); // False will send a restart, keeping the connection active
+  Wire.requestFrom(I2C_ADDR, 6, true);
+  // Accelerometer
+  accx = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  accy = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  accz = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(MAG_DATA_X_LSB_ADDR);
+  Wire.endTransmission(false);
+  Wire.requestFrom(I2C_ADDR, 26, true);
+  // Magnetometer
+  magx = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // mT
+  magy = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // mT
+  magz = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // mT
+  // Gyroscope
+  gyrox = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // Dps
+  gyroy = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // Dps
+  gyroz = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  // Dps
+  // Euler Angles
+  Yaw = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;    //in Degrees unit
+  Roll = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;   //in Degrees unit
+  Pitch = (int16_t)(Wire.read() | Wire.read() << 8) / 16.00;  //in Degrees unit
+  // Quaternions
+  q0 = (int16_t)(Wire.read() | Wire.read() << 8) / (pow(2, 14));  //unit less
+  q1 = (int16_t)(Wire.read() | Wire.read() << 8) / (pow(2, 14));  //unit less
+  q2 = (int16_t)(Wire.read() | Wire.read() << 8) / (pow(2, 14));  //unit less
+  q3 = (int16_t)(Wire.read() | Wire.read() << 8) / (pow(2, 14));  //unit less
+  //Convert Quaternions to Euler Angles
+  Yaw2 = (atan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (pow(q2, 2) + pow(q3, 2)))) * 180 / PI;
+  Roll2 = (asin(2 * (q0 * q2 - q3 * q1))) * 180 / PI;
+  Pitch2 = (atan2(2 * (q0 * q1 + q2 * q3), 1 - 2 * (pow(q1, 2) + pow(q2, 2)))) * 180 / PI;
+
+  //Linear (Dynamic) & Gravitational (Static) Acceleration
+  Wire.beginTransmission(I2C_ADDR);
+  Wire.write(LINEAR_ACCEL_DATA_X_LSB_ADDR);
+  Wire.endTransmission(false);
+  Wire.requestFrom(I2C_ADDR, 12, true);
+  LIAx = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  LIAy = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  LIAz = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  GRVx = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  GRVy = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+  GRVz = (int16_t)(Wire.read() | Wire.read() << 8) / 100.00;  // m/s^2
+
+  // Print data
+  Serial.print("Accel_X= ");
+  Serial.print(accx);
+  Serial.print(" Accel_Y= ");
+  Serial.print(accy);
+  Serial.print(" Accel_Z= ");
+  Serial.println(accz);
+}
