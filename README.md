165525828a8b9836216f814af25d39e8218e6a18
phf-soares <73134614+phf-soares@users.noreply.github.com>
2025-11-10 23:28:54 -0300

    Initialize http

---
+++ README.md
@@ -0,0 +1,83 @@
+# Projeto Monitoramento de Vibração na Saúde Humana
+
+Código compilado em Arduino IDE com biblioteca para esp32.
+
+Protocolo I2C entre:
+
+1) Módulo sensor BNO055
+
+![image](https://github.com/user-attachments/assets/376cb56f-e07f-475c-9e07-f9a22ab78216)
+
+2) ESP32-WROOM-32D OU DEVKIT-V1
+
+
+![image](https://github.com/user-attachments/assets/45c2b9af-bc6b-4dd6-a4b6-040efd92e512)
+
+Montagem:
+![image](https://github.com/user-attachments/assets/1353824b-a9ea-47d3-8f77-ff2a4f163949)
+
+Protocolo SPI entre ESP32 e:
+
+3) Módulo SD:
+
+
+![image](https://github.com/user-attachments/assets/3b610a80-6c44-49eb-a397-15c0167fdf9d)
+
+Montagem:
+![image](https://github.com/user-attachments/assets/ac5387a5-40da-4f57-bd0b-2beca50ef739)
+Feito por mim: https://wokwi.com/projects/434601176080578561
+
+*SD:*
+
+Tenho... 32GB...
+
+Alimentado pelo USB.
+
+Protocolo UART entre ESP32 e:
+
+4) Módulo GSM:
+
+![image](https://github.com/user-attachments/assets/16489fec-9d19-476c-877f-8954d327c72b)
+
+Fonte: https://www.youtube.com/watch?v=THCJWWsyh10
+
+Montagem:
+
+![image](https://github.com/user-attachments/assets/190d87d6-d7bf-4c1e-8c5d-8c385ba7c2de)
+Feito por mim: https://wokwi.com/projects/435414590476866561
+
+*Cartão SIM 2G:*
+
+Falta...
+
+ALIMENTAÇÃO:
+
+- Alimentação portátil por bateria e carregador da mesma:
+
+![image](https://github.com/user-attachments/assets/2655918b-aa83-43d6-be84-c99d01dfc830)
+
+Fonte: https://www.usinainfo.com.br/carregador-de-bateria/shield-v3-carregador-de-bateria-18650-com-usb-saida-5v-3v-e-protecao-de-sobrecarga-ou-descarga-excessiva-5319.html
+
+Exemplo da alimentação
+
+![image](https://github.com/user-attachments/assets/20aa0b39-3b69-42b3-8f20-2ac998f9312b)
+
+Fonte: https://youtu.be/J86yQlJO4w0?si=M3n3m-WgvZsvyjYb
+
+
+- Bateria recarregável:
+
+Bateria 18650 Lithium (Li-ion) 3.7V / 2600 mAh - FLEXGOLD FX-L2600 Flat-top
+
+![image](https://github.com/user-attachments/assets/967cf584-5a9a-4877-998e-19b579f174dc)
+
+
+Comunicação e display WEB -  enviando para ThingSpeak via GPRS
+
+Configuração de canal no ThingSpeak, teste com PuTTY do HTTP requests via GPRS.
+
+
+
+
+
+
---
+++ http/gprs_http_module/gprs_http_module.ino
@@ -0,0 +1,112 @@
+#include <SoftwareSerial.h> //Adiciona da biblioteca SoftwareSerial.h
+
+const int PORT_UART_RX = 16;
+const int PORT_UART_TX = 17;
+const int AMOSTRAGEM_MS = 1*1000; // duracao entre alertas em ms
+
+String apn = "internet";
+String apn_u = "";
+String apn_p = "";
+
+String url_string1 = "https://api.thingspeak.com/update?api_key=RN0VM260LCRNDMBR&field1";
+String url_string2 = "https://api.thingspeak.com/update?api_key=RN0VM260LCRNDMBR&field2";
+String url_string3 = "https://api.thingspeak.com/update?api_key=RN0VM260LCRNDMBR&field3";
+
+float x,y,z;
+long current_time, previous_time;
+String data_x, data_y, data_z;
+
+SoftwareSerial mySerial(PORT_UART_RX, PORT_UART_TX); //Cria objeto mySerial passando como parâmetro as portas digitais
+
+void setup() {
+  Serial.begin(9600); //Inicializa a comunicação serial  
+  init_GMS(); 
+  gsm_config_gprs(); // ativar GPRS
+}
+
+void loop() {
+  current_time = millis();
+  if (current_time - previous_time >= AMOSTRAGEM_MS) {
+
+    x = 12; // sensor values, hardcoded
+    y = 54;
+    z = 16;
+
+    data_x = String(x);
+    data_y = String(y);
+    data_z = String(z);
+
+    gms_http_post(url_string1, data_x);
+    gms_http_post(url_string2, data_y);
+    gms_http_post(url_string3, data_z);
+
+
+    previous_time = current_time;
+  }    
+}
+
+void gms_http_post (String url, String postdata) {
+  String sendURL = url+"="+postdata;
+  Serial.println(" --- Start GPRS & HTTP ---");
+  gsm_send_serial("AT+SAPBR=1,1"); //Opens the GPRS context to establish the connection. Expected response: "OK" if the connection is successfully established.
+  gsm_send_serial("AT+SAPBR=2,1"); // IP? Querying bearer?
+  gsm_send_serial("AT+HTTPINIT"); //Initializes the HTTP service on the module. Expected response: "OK" if the initialization is successful.
+  gsm_send_serial("AT+HTTPPARA=CID,1"); //Sets the HTTP context identifier to 1. Expected response: "OK" if the parameter is set successfully.
+  gsm_send_serial("AT+HTTPPARA=URL," + sendURL); //Sets the URL for the server with the actual URL of the server you want to send the GET request to. Expected response: "OK" if the URL is set successfully.
+  
+  //gsm_send_serial("AT+HTTPPARA=CONTENT,application/x-www-form-urlencoded"); //Indicates that the data will be in XXX format. Expected value: OK
+  gsm_send_serial("AT+HTTPDATA=192,5000"); //Prepares the module to receive the XXX data. Length, Time in ms to wait the data to be received.
+  gsm_send_serial(postdata); //Send just the XXX data. Expected response: OK If the number of characters are lower or higher than expected the command will fail with 'ERROR'.
+  gsm_send_serial("AT+HTTPACTION=1"); //antes (1) Initiates the HTTP POST request. Expected response: "OK", number of characters in HTTP response followed by the HTTP response code (e.g. "+HTTPACTION:42,201")
+  gsm_send_serial("AT+HTTPREAD"); //Reads the HTTP response from the server. Expected response: The response data from the server. usually contains the json data you just transmitted with some additional details or error information in case the request returns anything other than 200 or 201 status code.
+  gsm_send_serial("AT+HTTPTERM"); //Terminates the HTTP service on the module. Expected response: "OK" if the termination is successful.
+  gsm_send_serial("AT+HTTPSAPBR=0,1"); // Closing bearer?
+}
+
+void gsm_config_gprs() {
+  Serial.println("--- CONFIG GPRS ---");
+  gsm_send_serial("AT+SAPBR=3,1,Contype,GPRS"); //Sets the connection type to GPRS. Expected response: "OK" if the command is successful.
+  gsm_send_serial("AT+SAPBR=3,1,APN" + apn); //Sets the Access Point Name (APN) for the GPRS connection. APN for your network provider. Expected response: "OK" if the command is successful.
+  if (apn_u != "") {
+    gsm_send_serial("AT+SAPBR=3,1,USER" + apn_u);
+  }
+  if (apn_p != "") {
+    gsm_send_serial("AT+SAPBR=3,1,PWD" + apn_p);
+  }
+}
+
+void gsm_send_serial (String command) {
+  Serial.println("Send ->:" + command);
+  mySerial.println(command);
+  read_GMS();
+  Serial.println();
+}
+
+void init_GMS() {
+  mySerial.begin(9600); //Inicializa comunicação entre o SIM800L e o ESP32
+  delay(1000);  //Intervalo de 1 segundo
+  Serial.println("--- TESTE AT ---");
+  mySerial.println("AT"); //Teste de conexão, espera OK
+  read_GMS(); //Chamada da função read_GMS()
+  delay(30000);
+}
+
+void read_GMS() {  
+  while (mySerial.available()) //Verifica se a comunicação serial está disponível
+  {
+    Serial.write(mySerial.read()); //Realiza leitura serial dos dados de entrada ESP32
+  }
+  delay(500); //Intervalo de 0,5 segundos
+}
+
+void gsm_send_serial2 (String command) {
+  Serial.println("Send ->:" + command);
+  mySerial.println(command);
+  long wtimer = millis();
+  while (wtimer + 3000 > millis()) {
+    while (mySerial.available()) {
+      Serial.write(mySerial.read());
+    }
+  }
+  Serial.println();
+}
